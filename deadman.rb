9351671f4487c9498a264d4ff96f7f1ff85d5b97
KOSHIKAWA Kenichi <reishoku.misc@pm.me>
2022-05-03 18:13:16 +0900

    modified installation processes: purged venv setup
    Signed-off-by: KOSHIKAWA Kenichi <reishoku.misc@pm.me>

--- deadman.rb
+++ deadman.rb
@@ -1,5 +1,4 @@
 class Deadman < Formula
-  include Language::Python::Virtualenv
 
   desc "deadman is a curses-based host status checking application using ping"
   homepage "https://github.com/upa/deadman"
@@ -10,19 +9,11 @@ class Deadman < Formula
   depends_on "python3"
 
   def install
-    venv = virtualenv_create(libexec, "python3")
-
-    # install deadman commands with PATH set to Python virtualenv environment
     bin.install Dir[buildpath/"deadman"]
     share.install Dir[buildpath/"deadman.conf"]
-    # venv.pip_install_and_link buildpath
   end
 
-  # def post_install
-  #   system Formula["python3"].opt_bin/"python3", *Language::Python.setup_install_args(prefix)
-  # end
-
   test do
-    system "#{bin}/deadman", "--version"
+    system "#{bin}/deadman", "--help"
   end
 end
