8fa7262e0755ee4bea5e0c6de41dd0efff26d367
KOSHIKAWA Kenichi <reishoku.misc@pm.me>
2022-05-03 18:07:39 +0900

    modified installation processes: purged unneeded comments
    Signed-off-by: KOSHIKAWA Kenichi <reishoku.misc@pm.me>

--- deadman.rb
+++ deadman.rb
@@ -9,28 +9,8 @@ class Deadman < Formula
 
   depends_on "python3"
 
-  # resource "requests" do
-  #   url "https://pypi.io/packages/source/r/requests/requests-2.27.1.tar.gz"
-  #   sha256 "68d7c56fd5a8999887728ef304a6d12edc7be74f1cfa47714fc8b414525c9a61"
-  # end
-
-  # resource "chardet" do
-  #   url "https://pypi.io/packages/source/c/chardet/chardet-4.0.0.tar.gz"
-  #   sha256 "0d6f53a15db4120f2b08c94f11e7d93d2c911ee118b6b30a04ec3ee8310179fa"
-  # end
-
   def install
     venv = virtualenv_create(libexec, "python3")
-    venv.pip_install resources
-
-    # system "#{libexec}/bin/python", "setup.py",
-    #                  "configure",
-    #                  "--prefix=#{libexec}"
-    # system "#{libexec}/bin/python", "setup.py",
-    #                  "--no-user-cfg",
-    #                  "--no-update-icon-cache",
-    #                  "--no-compile-schemas",
-    #                  "install"
 
     # install deadman commands with PATH set to Python virtualenv environment
     bin.install Dir[bin/"deadman"]
